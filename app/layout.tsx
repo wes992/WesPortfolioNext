d6009d3071b54ec6672b5a227c01ccac055eb4eb
Wes Hampton <wes.hampton@infiniteblue.com>
2024-07-31 23:57:46 -0400

    add noto font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+//👇 Configure our font object
+const notoSanse = Noto_Sans_Mono({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={notoSanse.className}>{children}</body>
     </html>
   );
 }
