413043efdc7a866e36d534fe07924ab0ad6a5188
Wes Hampton <wes.hampton@infiniteblue.com>
2024-07-31 23:59:28 -0400

    add portfolio page

---
+++ app/components/Portfolio/Portfolio.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Portfolio.module.css";
+import { Header } from "../Header";
+
+const Portfolio = ({}) => {
+  return (
+    <div className={styles.container}>
+      <Header number={"02"} title="Portfolio" subtitle="My Latest Work:" />
+      <div className={styles.projects}>
+        <div
+          className={styles.project}
+          style={{ backgroundColor: "white" }}
+        ></div>
+        <div
+          className={styles.project}
+          style={{ backgroundColor: "red" }}
+        ></div>
+        <div
+          className={styles.project}
+          style={{ backgroundColor: "blue" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export { Portfolio };
