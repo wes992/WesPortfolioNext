d0b5186f7f3099e005d66e69f0698a49ab2f2199
Wes Hampton <wes.hampton@infiniteblue.com>
2024-08-02 22:02:46 -0400

    add nav

--- app/page.tsx
+++ app/page.tsx
@@ -6,26 +6,47 @@ import { TagLine } from "./components/TagLine";
 import { Skills } from "./components/Skills/Skills";
 import { useRef } from "react";
 import { Portfolio } from "./components/Portfolio";
+import { Experience } from "./components/Experience";
+import Link from "next/link";
+import { Contact } from "./components/Contact";
+import { Logo } from "./components/icons/Logo";
 
 export default function Home() {
-  const ref = useRef(null);
-  useScrollEffect(ref);
+  const landingRef = useRef(null);
+  useScrollEffect(landingRef);
+
+  // const skillsRef = useRef(null);
+  // useScrollEffect(skillsRef, 0.1, { threshold: 0.1 });
   return (
-    <main className={styles.main}>
-      <section className={styles.section} ref={ref} id="section1">
-        <nav>
-          <a href="#skills">skills</a>
-          <a href="#portfolio">Portfolio</a>
-        </nav>
-        <TagLine text={"I'm Wes Hampton."} />
-      </section>
-      <Skills />
-      <section className={styles.section} id="portfolio">
-        <Portfolio />
-      </section>
-      <section className={styles.section} id="section4">
-        <h2>Section4</h2>
-      </section>
-    </main>
+    <>
+      <nav className={styles.nav}>
+        <Link href="/">
+          <Logo height="40px" width="40px" color={"rgb(var(--accent-color))"} />
+        </Link>
+        <div className={styles.links}>
+          <Link href="#skills">skills</Link>
+          <Link href="#portfolio">Portfolio</Link>
+          <Link href="#experience">experience</Link>
+          <Link href="#contact">contact</Link>
+        </div>
+      </nav>
+      <main className={styles.main}>
+        <section className={styles.section} ref={landingRef} id="section1">
+          <TagLine text={"I'm Wes Hampton."} />
+        </section>
+        <section className={styles.section} id="skills">
+          <Skills />
+        </section>
+        <section className={styles.section} id="portfolio">
+          <Portfolio />
+        </section>
+        <section className={styles.section} id="experience">
+          <Experience />
+        </section>
+        <section className={styles.section} id="contact">
+          <Contact />
+        </section>
+      </main>
+    </>
   );
 }
